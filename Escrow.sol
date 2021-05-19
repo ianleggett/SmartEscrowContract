4913d714c5928850d6f1e8da332822479fd80c27
ianleggett <ian_on_drums@yahoo.co.uk>
2021-05-19 20:30:27 +0100

    Add files via upload
    Updates

--- Escrow.sol
+++ Escrow.sol
@@ -16,15 +16,13 @@ contract Escrow is Ownable {
     enum PaymentStatus { Unknown, Created, Agreed, Deposit, GoodsSent, GoodsReceived, Completed, RequestCancel, CancelAgreed, Refunded, Arbitration, Error }
 
     /// Only the buyer can call this function.
-    error OnlyBuyer();
+    //error OnlyBuyer();
     /// Only the seller can call this function.
-    error OnlySeller();
+    //error OnlySeller();
     /// The function cannot be called at the current state.
     error InvalidState();
 
-    error FailedPayment();
-
-    error NotImplementedYet();
+    error FailedPayment(string);
 
     uint256 public feesAvailable;  // summation of fees that can be withdrawn
 
@@ -39,15 +37,32 @@ contract Escrow is Ownable {
         bool refundApproved;
     }
 
-    modifier onlyBuyer(uint _orderId) {
-        if (msg.sender != agrements[_orderId].buyer)
-            revert OnlyBuyer();
+    modifier onlyBuyer(uint _orderId){
+        require(msg.sender == agrements[_orderId].buyer,"Only Buyer can call this");
         _;
     }
+
     modifier onlySeller(uint _orderId) {
-        if (msg.sender != agrements[_orderId].buyer)
-            revert OnlySeller();
+       require(msg.sender == agrements[_orderId].seller,"Only Seller can call this");
         _;
+    }
+
+    function getState(uint _orderId) public view returns (PaymentStatus){
+        Payment storage _payment = agrements[_orderId];
+        return _payment.status;
+    }
+
+    function getContractBalance() public view returns (uint256){
+        return currency.balanceOf(address(this));
+    }
+
+    function getAllowance(uint _orderId) public view returns (uint256){
+        Payment storage _payment = agrements[_orderId];
+        return currency.allowance(_payment.buyer,address(this));
+    }
+
+    function getVersion() public pure returns (string memory){
+        return "Escrow V1.05";
     }
 
     mapping(uint => Payment) public agrements;
@@ -75,35 +90,50 @@ contract Escrow is Ownable {
     /**
        Fund transferred in from Buyer
     **/
-    function deposit(uint _orderId) external onlyBuyer(_orderId) {
+    function deposit(uint _orderId) external payable onlyBuyer(_orderId) {
         Payment storage _payment = agrements[_orderId];
         require(_payment.status!=PaymentStatus.Unknown, "Agreement does not exist");
-        if (currency.transfer(_payment.buyer,_payment.value)){
+        // buyer must approve the value amount beforehand in Dapp
+        // how much has the buyer allowed us to get
+        require(currency.allowance(_payment.buyer,address(this)) >= _payment.value,"Buyer needs to approve funds to Escrow first !!");
+        if (currency.transferFrom(_payment.buyer,payable(address(this)),_payment.value)){
             _payment.status =  PaymentStatus.Deposit;
             emit PaymentDeposit(_orderId, _payment);
         }else{
             emit PaymentFail(_orderId, _payment);
-            revert FailedPayment();
+            revert FailedPayment("Failed");
         }
+    }
+
+    function goodsSent(uint _orderId) external onlySeller(_orderId){
+        Payment storage _payment = agrements[_orderId];
+        require(_payment.status == PaymentStatus.Deposit,"Need to deposit funds first !!");
+        _payment.status = PaymentStatus.GoodsSent;
+        emit PaymentGoodsReceived(_orderId, _payment);
     }
 
     function goodsReceived(uint _orderId) external onlyBuyer(_orderId){
         Payment storage _payment = agrements[_orderId];
-        require(_payment.status == PaymentStatus.GoodsReceived);
+        require(_payment.status == PaymentStatus.GoodsSent,"Goods have not been sent");
+        _payment.status = PaymentStatus.GoodsReceived;
+        // here we tell the curreny that the seller can ONLY have 'value' funds.
+        require(currency.approve(_payment.seller,_payment.value),"Can not approve sellers funds !!");
         emit PaymentGoodsReceived(_orderId, _payment);
-        completePayment(_orderId, _payment);
     }
 
-    function completePayment(uint _orderId, Payment storage _payment) private {
-        require( _payment.status == PaymentStatus.GoodsReceived);
-        uint256 _totalFees = _payment.fee + _payment.additionalGasFees;
+     /// release funds to the seller
+    function paySeller(uint _orderId) external payable {
+        Payment storage _payment = agrements[_orderId];
+        require(msg.sender == owner(),"Only Owner can release funds ");
+        require( _payment.status == PaymentStatus.GoodsReceived,"Goods have not been received");
+        uint256 _totalFees = _payment.fee; // + _payment.additionalGasFees;
         feesAvailable += _totalFees;
-        payable(_payment.seller).transfer( _payment.value - _totalFees);
+        currency.transfer(_payment.seller, _payment.value - _totalFees);
         _payment.status = PaymentStatus.Completed;
         emit PaymentComplete(_orderId, _payment);
     }
 
-    function withdrawFees(address payable _to, uint256 _amount) onlyOwner external {
+    function withdrawFees(address payable _to, uint256 _amount) external payable {
         // This check also prevents underflow
         require(_amount <= feesAvailable, "Amount is higher than amount available");
         feesAvailable -= _amount;
@@ -112,16 +142,14 @@ contract Escrow is Ownable {
 
     /// TO DO: Implement this, remove 'pure' and require() to stop compiler warnings
     function refund(uint _orderId) pure external {
-        require(_orderId==_orderId); // stop compiler warning until we implement
-        revert NotImplementedYet();
+        require(_orderId!=_orderId,"Not Implemented Yet!!"); // stop compiler warning until we implement
         //Payment storage _payment = agrements[_orderId];
         //refundPayment(_orderId, msg.sender, PaymentStatus.Refunded);
     }
 
     /// TO DO: Implement this, remove 'pure' and require() to stop compiler warnings
     function approveRefund(uint _orderId) pure external {
-        require(_orderId==_orderId); // stop compiler warning until we implement
-        revert NotImplementedYet();
+        require(_orderId!=_orderId,"Not Implemented Yet!!"); // stop compiler warning until we implement
         // Payment storage _payment = agrements[_orderId];
         // require(msg.sender == _payment.seller);
         // _payment.refundApproved = true;
@@ -129,6 +157,10 @@ contract Escrow is Ownable {
 
     function increaseGasCosts(uint _orderId, uint128 _gas) private {
         agrements[_orderId].additionalGasFees += _gas * uint128(tx.gasprice);
+    }
+
+    function notImplementedYet() pure private {
+        require(1!=2,"Not Implemented Yet!!"); // stop compiler warning until we implement
     }
 
 }
