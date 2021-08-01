7ab9ff6acbd0cd83f7c7b5f1c0147c7620abdbbf
ianleggett <ian_on_drums@yahoo.co.uk>
2021-08-01 11:20:45 +0100

    Updated to reflect refund and arbitration scenarios

--- TinanceEscrow.sol
+++ TinanceEscrow.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: Apache 2.0
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/token/ERC20/utils/SafeERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/access/Ownable.sol";
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/token/ERC20/utils/SafeERC20.sol";
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/access/Ownable.sol";
 
 // local build
-//import "../../openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
-//import "../../openzeppelin-contracts/contracts/access/Ownable.sol";
+import "../../openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../../openzeppelin-contracts/contracts/access/Ownable.sol";
 
 
 contract TinanceEscrow is Ownable {
@@ -18,9 +18,10 @@ contract TinanceEscrow is Ownable {
     event EscrowTokenSent(uint indexed orderId,Escrow escrow);
     event EscrowTokenReceived(uint indexed orderId,Escrow escrow);
     event EscrowComplete(uint indexed orderId, Escrow escrow);
+    event EscrowRefund(uint indexed orderId);
     event EscrowDisputeResovled(uint indexed orderId);
 
-    enum EscrowStatus { Unknown, Funded, TokenApproved, FiatReceived, Completed, RequestCancel, CancelAgreed, Refunded, Arbitration, Error }
+    enum EscrowStatus { Unknown, Funded, TokenApproved, Completed, Refund, Arbitration }
 
     uint256 public feesAvailable;  // summation of fees that can be withdrawn
     
@@ -29,10 +30,10 @@ contract TinanceEscrow is Ownable {
         address payable seller;
         uint256 value;
         uint128 additionalGasFees;
-        uint256 fee;
-        uint32 expiry;
-        EscrowStatus status;
-        bool refundApproved;
+        uint256 sellerfee;
+        uint256 buyerfee;
+        EscrowStatus status;       
+        //uint32 expiry;
     }
     
     mapping(uint => Escrow) public escrows;
@@ -72,9 +73,14 @@ contract TinanceEscrow is Ownable {
         return _escrow.value;
     }
     
-    function getFee(uint _orderId) public view returns (uint256){
+    function getSellerFee(uint _orderId) public view returns (uint256){
         Escrow memory _escrow = escrows[_orderId];
-        return _escrow.fee;
+        return _escrow.sellerfee;
+    }
+    
+    function getBuyerFee(uint _orderId) public view returns (uint256){
+        Escrow memory _escrow = escrows[_orderId];
+        return _escrow.buyerfee;
     }
     
     function getAllowance(uint _orderId) public view returns (uint256){
@@ -83,36 +89,43 @@ contract TinanceEscrow is Ownable {
     }
 
     function getVersion() public pure returns (string memory){
-        return "Escrow V1.196";
+        return "Escrow V1.198";
     }
 
-    function createEscrow(uint _orderId, address payable _buyer, address payable _seller, uint _value, uint _fee, uint32 _expiry) external onlyOwner {
+    function getCCY() public view returns (IERC20){
+        return tokenccy;
+    }
+    
+    function getFeesAvailable() public view onlyOwner returns (uint256){
+        return feesAvailable;
+    }
+
+    function createEscrow(uint _orderId, address payable _buyer, address payable _seller, uint _value, uint _sellerfee, uint _buyerfee) external onlyOwner {
         require(escrows[_orderId].status == EscrowStatus.Unknown, "Escrow already exists");
         
-       // require(msg.value >= getValue(_orderId), "The amount and the value sent to deposit do not match");
-        Escrow memory _escrow =  Escrow(_buyer, _seller, _value, /*gas*/0, _fee, _expiry, EscrowStatus.Funded, false);
-        escrows[_orderId] = _escrow;
-        
-         //Transfer USDT to contract after escrow creation
+        //Transfer USDT to contract after escrow creation
         require( tokenccy.allowance( _seller, address(this)) >= (_value),"Seller needs to approve funds to Escrow first !!");
+       
+        Escrow memory _escrow =  Escrow(_buyer, _seller, _value, /*gas*/0, _sellerfee, _buyerfee, EscrowStatus.Unknown);        
+        
         tokenccy.safeTransferFrom(_seller, address(this) , (_value) );
+         _escrow.status = EscrowStatus.Funded;
+        escrows[_orderId] = _escrow;
         tokenccy.safeApprove(_escrow.buyer,0); // reset any allowances
-         
+
         emit EscrowDeposit(_orderId, _escrow);
      }
 
-    function releaseEscrow(uint _orderId) external onlySeller(_orderId){
-        Escrow memory _escrow = escrows[_orderId];
-        require(_escrow.status == EscrowStatus.Funded,"USDT has not been deposited");
-       
+    function releaseEscrow(uint _orderId) external onlySeller(_orderId){ 
+         Escrow memory _escrow = escrows[_orderId];     
+        require(escrows[_orderId].status == EscrowStatus.Funded,"USDT has not been deposited");         
         _escrow.status = EscrowStatus.TokenApproved;
         
-        uint256 _totalFees = _escrow.fee + _escrow.additionalGasFees;
+        uint256 _totalFees = _escrow.sellerfee + _escrow.buyerfee + _escrow.additionalGasFees;
         feesAvailable += _totalFees;
         
         // here we tell the curreny that the buyer can ONLY have 'value' funds.
         tokenccy.safeApprove(_escrow.buyer,(_escrow.value - _totalFees));
-        //emit EscrowComplete(_orderId, _escrow);
         
         require(_escrow.status == EscrowStatus.TokenApproved,"USDT has not been approved!");
         _escrow.status = EscrowStatus.Completed;
@@ -120,46 +133,83 @@ contract TinanceEscrow is Ownable {
         tokenccy.safeTransfer( _escrow.buyer, (_escrow.value - _totalFees) );
         delete escrows[_orderId];
         emit EscrowComplete(_orderId, _escrow);
- 
     }
 
-     /// release funds to the buyer in case of dispute
-    function refundEscrow(uint _orderId, uint8 _buyerPercent) external onlyOwner {
+    function approveRefund(uint _orderId) external onlyOwner {
         Escrow memory _escrow = escrows[_orderId];
-        require( _escrow.status == EscrowStatus.FiatReceived,"Fiat money have not been received on seller side!");
-        uint256 _totalFees = _escrow.fee  + _escrow.additionalGasFees;
-        feesAvailable += _totalFees;
-        
+        require(_escrow.status == EscrowStatus.Funded,"USDT has not been deposited");
+         _escrow.status = EscrowStatus.Refund;
+         escrows[_orderId] = _escrow;
+    }
+
+     /// release funds to the seller - cancelled contract
+    function refundSeller(uint _orderId) external onlyOwner {
+        Escrow memory _escrow = escrows[_orderId];
+        require(_escrow.status == EscrowStatus.Refund,"Refund not approved");        
+                
+        // dont charge seller any fees - because its a refund
+        uint256 _totalFees = _escrow.additionalGasFees;
+        feesAvailable += _totalFees; // arb deduct fees , full refund no fees // arbfee 0.2%
+        uint256 amtReturn = (_escrow.value - _totalFees);
+         
+        if (amtReturn > 0){
+            tokenccy.safeTransfer(_escrow.seller, amtReturn);
+        }
         delete escrows[_orderId];
         emit EscrowDisputeResovled(_orderId);
-        
-        if (_buyerPercent > 0)
-            tokenccy.safeTransfer(_escrow.buyer, (_escrow.value - _totalFees) * _buyerPercent / 100);
-        if (_buyerPercent < 100)
-            tokenccy.safeTransfer(_escrow.seller, (_escrow.value - _totalFees) * (100 - _buyerPercent) / 100);
      }
 
-    function withdrawFees(address payable _to, uint256 _amount) external payable onlyOwner {
+    function setArbitration(uint _orderId) external view onlyOwner {
+        Escrow memory _escrow = escrows[_orderId];
+        require(_escrow.status == EscrowStatus.Funded,"Can not Arbitrate, USDT has not been deposited");
+         _escrow.status = EscrowStatus.Arbitration;
+    }
+
+     /// release funds to the seller/buyer in case of dispute
+    function arbitrationEscrow(uint _orderId, uint8 _buyerPercent) external onlyOwner {
+        require( (_buyerPercent >= 0 && _buyerPercent <= 100),"Buyer percent out of range");
+        Escrow memory _escrow = escrows[_orderId];
+        require(_escrow.status == EscrowStatus.Funded,"USDT has not been deposited");
+        
+       // require( (_escrow.status == EscrowStatus.Refund) || (_escrow.status == EscrowStatus.Arbitration) ,"Must be in correct state!");
+        
+        uint256 _totalFees = _escrow.sellerfee  + _escrow.buyerfee + _escrow.additionalGasFees;
+        feesAvailable += _totalFees; // arb deduct fees , full refund no fees
+        
+        uint256 amtReturn = (_escrow.value - _totalFees);
+        uint256 amtBuyer = (amtReturn * _buyerPercent) / 100;
+        uint256 amtSeller = amtReturn - amtBuyer;
+        if (amtBuyer > 0){
+            tokenccy.safeTransfer(_escrow.buyer, amtBuyer);
+        }
+        if (amtSeller > 0){
+            tokenccy.safeTransfer(_escrow.seller, amtSeller);
+        }
+        delete escrows[_orderId];
+        emit EscrowDisputeResovled(_orderId);
+     }
+
+    function withdrawFees(address payable _to, uint256 _amount) external onlyOwner {
         // This check also prevents underflow
-        require(_amount <= feesAvailable, "Amount is higher than amount available");
+        require(_amount <= feesAvailable, "Amount is higher than feesAvailable");
         feesAvailable -= _amount;
         tokenccy.safeTransfer( _to, _amount );
     }
 
     /// TO DO: Implement this, remove 'pure' and require() to stop compiler warnings
-    function refund(uint _orderId) pure external {
-        require(_orderId!=_orderId,"Not Implemented Yet!!"); // stop compiler warning until we implement
-        //Payment storage _payment = agrements[_orderId];
-        //refundPayment(_orderId, msg.sender, PaymentStatus.Refunded);
-    }
+    // function refund(uint _orderId) pure external {
+    //     require(_orderId!=_orderId,"Not Implemented Yet!!"); // stop compiler warning until we implement
+    //     //Payment storage _payment = agrements[_orderId];
+    //     //refundPayment(_orderId, msg.sender, PaymentStatus.Refunded);
+    // }
 
     /// TO DO: Implement this, remove 'pure' and require() to stop compiler warnings
-    function approveRefund(uint _orderId) pure external {
-        require(_orderId!=_orderId,"Not Implemented Yet!!"); // stop compiler warning until we implement
-        // Payment storage _payment = agrements[_orderId];
-        // require(msg.sender == _payment.seller);
-        // _payment.refundApproved = true;
-    }
+    // function approveRefund(uint _orderId) pure external {
+    //     require(_orderId!=_orderId,"Not Implemented Yet!!"); // stop compiler warning until we implement
+    //     // Payment storage _payment = agrements[_orderId];
+    //     // require(msg.sender == _payment.seller);
+    //     // _payment.refundApproved = true;
+    // }
 
     function increaseGasCosts(uint _orderId, uint128 _gas) private {
         escrows[_orderId].additionalGasFees += _gas * uint128(tx.gasprice);
